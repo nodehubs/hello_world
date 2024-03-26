f9cc9fcf8a9a5c14fca72cd0a08521976380d083
xuguosheng110 <xgs11@qq.com>
2024-03-26 21:42:24 +0800

    fist pull

---
+++ build/hello_word/CMakeFiles/hello_word_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_word_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hello_word_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
